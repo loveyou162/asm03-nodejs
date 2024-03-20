09b54f13ab7ecd5db35d2bef4397bf5404fa5798
Phạm Đình Thắng <89562712+loveyou162@users.noreply.github.com>
2024-03-20 23:17:06 +0700

    Update shop.js

--- controller/shop.js
+++ controller/shop.js
@@ -30,7 +30,7 @@ const formatPrice = (price) => {
 exports.getAllProducts = async (req, res, next) => {
   Product.find()
     .then((result) => {
-      // console.log(result);
+      console.log(req.session);
       return res.json(result);
     })
     .catch((err) => {
