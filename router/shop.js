b590499303c9a8f378e6e9b1fff727069c334468
Pham Thang <caoboi520@gmail.com>
2024-03-20 22:04:17 +0700

    test-auth

--- router/shop.js
+++ router/shop.js
@@ -8,42 +8,42 @@ const { body } = require("express-validator");
 const User = require("../models/user");
 router.get(
   "/all-product",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.getAllProducts
 );
 router.post(
   "/add-cart",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.postCart
 );
 router.get(
   "/cart",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.getCart
 );
 router.post(
   "/delete-cart",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.postDeleteCartProduct
 );
 router.post(
   "/decrement-cart",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.postDecrementCart
 );
 router.post(
   "/order",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.postOrder
 );
 router.get(
   "/order",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.getOrder
 );
 router.post(
   "/order-detail",
-  [checkAuth.checkClientAuth, checkTypeUser.checkClient],
+  // [checkAuth.checkClientAuth, checkTypeUser.checkClient],
   shopController.postOrderDetail
 );
 router.post(
